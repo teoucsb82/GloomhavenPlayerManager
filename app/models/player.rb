ee15cff5c885aa964e91cb45d3f55d4d992fb2ad
Teo Dell'Amico <teo.dellamico@gmail.com>
2020-03-09 20:07:19 -0700

    Added basic player show

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,7 +1,7 @@
 class Player < ApplicationRecord
   belongs_to :user
 
-  # validates_presence_of :name, :character_class, :gold, :xp
-  # validates_numericality_of :gold, :xp, greater_than_or_equal_to: 0
+  validates_presence_of :name, :character_class, :gold, :xp
+  validates_numericality_of :gold, :xp, greater_than_or_equal_to: 0
 
 end
